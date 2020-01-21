01aef7845dfabfc4a278188067c8a51c01d061bf
W-David <1776867536@qq.com>
2020-01-21 13:33:50 +0800

    update on 2020-1-21 (HeadOfAGang)

--- HeadOfAGang/main.cpp
+++ HeadOfAGang/main.cpp
@@ -1,29 +1,30 @@
 #include <bits/stdc++.h>
 using namespace std;
 vector<string> name;
-map<string,int> index;
-int gang[1005][1005];
-int weightSum = 0,maxWeight = -1,head = -1,cnt = 0;
-int weight[1005] = {0},vis[1005] = {0};
+map<string,int> nameIndex;
+const static int maxn = 2020;
+int gang[maxn][maxn];
+int weightSum = 0,maxWeight = 0,head = 0,cnt = 0;
+int weight[maxn] = {0},vis[maxn] = {0};
 void save(const string& s){
-    auto it = index.find(s);
-    if(it == index.end()){
+    auto it = nameIndex.find(s);
+    if(it == nameIndex.end()){
         name.push_back(s);
-        index[s] = name.size() - 1;
+        nameIndex[s] = name.size() - 1;
     }
 }
 void dfs(int v){
     cnt++;
     vis[v] = 1;
-    if(weight[v] > maxWeight){
-        maxWeight = weight[v];
-        head = v;
-    }
     for(int w = 0;w < name.size();w++){
         if(!gang[v][w]) continue;
         weightSum += gang[v][w];
         gang[v][w] = gang[w][v] = 0;
         if(!vis[w]) dfs(w);
+    }
+    if(weight[v] > maxWeight){
+        maxWeight = weight[v];
+        head = v;
     }
 }
 int main() {
@@ -33,13 +34,13 @@ int main() {
     while(n--){
         cin>>nameA>>nameB>>len;
         save(nameA),save(nameB);
-        int v = index[nameA],w = index[nameB];
+        int v = nameIndex[nameA],w = nameIndex[nameB];
         weight[v] += len,weight[w] += len;
         gang[v][w] += len,gang[w][v] += len;
     }
-    vector<pair<string,int>> vc;
+    vector<pair<string,int> > vc;
     for(int i = 0;i < name.size();i++){
-        weightSum = 0,maxWeight = -1,head = -1,cnt = 0;
+        weightSum = maxWeight = head = cnt = 0;
         if(!vis[i]) dfs(i);
         if(cnt > 2 && weightSum > limit) vc.emplace_back(name[head],cnt);
     }
--- StudentList/main.cpp
+++ StudentList/main.cpp
@@ -1,26 +1,31 @@
-#include <cstdio>
+#include <iostream>
+#include <string>
 #include <vector>
 #include <algorithm>
-#include <cstring>
 using namespace std;
-char Sid[40005][5];
-vector<int> course[2505];
-int main() {
-    int sc,cc;
-    scanf("%d %d",&sc,&cc);
-    for(int i = 1;i <= sc;i++){
-        int cnt,cid;
-        scanf("%s %d",Sid[i],&cnt);
-        for(int k = 0;k < cnt;k++){
-            scanf("%d",&cid);
-            course[cid].push_back(i);
+struct vc{
+    vector<string> vmp;
+}mp[2505];
+void store(int n){
+    for(int i=0;i < n;i++){
+        string studentId;cin>>studentId;
+        int c;cin>>c;
+        while(c--){
+            int courseId;cin>>courseId;
+            mp[courseId].vmp.push_back(studentId);
         }
     }
-    for(int i = 1;i <= cc;i++){
-        auto cv = course[i];
-        printf("%d %d\n",i,cv.size());
-        sort(cv.begin(),cv.end(),[](int a,int b){return strcmp(Sid[a],Sid[b]) < 0;});
-        for(auto &it : cv) printf("%s\n",Sid[it]);
+}
+void solve(int m){
+    for(int i=1;i <= m;i++){
+       cout<<i<<" "<<mp[i].vmp.size()<<endl;
+       sort(mp[i].vmp.begin(),mp[i].vmp.end());
+       for(auto &it : mp[i].vmp) cout<<it<<endl;
     }
+}
+int main() {
+    int n,m; cin>>n>>m;
+    store(n);
+    solve(m);
     return 0;
 }
